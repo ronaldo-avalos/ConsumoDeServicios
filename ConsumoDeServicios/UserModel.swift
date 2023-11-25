5488125ef2e0260f26dd33ad23f90896b6480d4f
Ronalo Avalos <ronaldoadan1@gmail.com>
2023-11-25 17:52:00 -0600

    commit initial

---
+++ ConsumoDeServicios/UserModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserModel.swift
+//  ConsumoDeServicios
+//
+//  Created by Ronaldo Avalos on 25/11/23.
+//
+
+import Foundation
+
+
+struct UserModel: Codable {
+    let user : String
+    let age : Int
+    let isHappy : Bool
+}
--- ConsumoDeServicios/ViewController.swift
+++ ConsumoDeServicios/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameLabel : UILabel!
+    @IBOutlet weak var stateLabel : UILabel!
+    @IBOutlet weak var loadig : UIActivityIndicatorView!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchService()
     }
-
+    
+    
+    // Endpoint: https://run.mocky.io/v3/0a1b574e-0ccb-4996-863c-b042023975e5
+       // 1. Crear expeción de seguridad - ✅
+       // 2. Crear URL con el endpoint. - OK
+       // 3. Hacer request con la ayuda de URLSession
+       // 4. Transformar respuesta a diccionario
+       // 5. Ejecutar Request
+    private func fetchService() {
+           let endpointString = "https://run.mocky.io/v3/0a1b574e-0ccb-4996-863c-b042023975e5"
+           
+           guard let endpoint = URL(string: endpointString) else {
+               return
+           }
+           
+        loadig.startAnimating()
+           URLSession.shared.dataTask(with: endpoint) { (data: Data?, _, error: Error?) in
+               
+               DispatchQueue.main.async {
+                   self.loadig.stopAnimating()
+               }
+               
+               if error != nil {
+                   print("Hubo un error!")
+                   
+                   return
+               }
+               
+               guard 
+                let dataFromService = data,
+                let model = try? JSONDecoder().decode(UserModel.self, from: dataFromService) else {
+                   return
+               }
+                    
+                    
+               DispatchQueue.main.async {
+                   self.usernameLabel.text = model.user
+                   self.stateLabel.text = model.isHappy ? "Es feliz 😁" : "Es triste 😔"
+               }
+           }.resume()
+       }
 
 }
 
